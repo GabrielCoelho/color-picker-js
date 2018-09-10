7015cb6bf5c5b3128280a5e4484b0cb8b15a31a6
Gabriel Coelho Soares <soares@workmail.com>
2018-09-10 09:00:03 -0300

    Starting the Script editing

---
+++ script.js
@@ -0,0 +1,27 @@
+let colorToFind = document.querySelector("#colorToFind");
+let innerContent = document.querySelector("#innerContent");
+let newGame = document.querySelector("#new");
+let easyGame = document.querySelector("#easy");
+let normalGame = document.querySelector("#normal");
+let hardGame = document.querySelector("#hard");
+
+/** for loop to get all div colors */
+let color = Array();
+for(cl=1; cl<10; cl++){
+    color[cl] = document.querySelector("#color"+[cl]);
+    // console.log(color[cl]);
+}
+
+
+function gameInitialized(){
+    console.log("Game Initialized");
+    /**For loop 9 colors (number of boxes) */
+    for(i=0; i<10; i++){
+        /** For loop 3 define color (RGB parameters) */
+        for(j=0; j<3; j++){
+            let red = Math.floor(Math.random() * 255);
+            let blue = Math.floor(Math.random() * 255);
+            let green = Math.floor(Math.random() * 255);
+        }
+    }
+}
